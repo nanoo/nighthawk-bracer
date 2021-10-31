d3a016fe3c75a751737e1216945b46943cc20677
nanoo <tempi@mac.com>
2021-10-31 17:40:33 -0400

    feat: Add center color wheel (#3)

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 import time
 import board
 import pwmio
+import neopixel
 
 # PWM (fading) LEDs are connected on D2
 pwm_leds = board.D2
@@ -8,6 +9,12 @@ pwm = pwmio.PWMOut(pwm_leds, frequency=1000, duty_cycle=0)
 counter = 0  # counter to keep track of cycles
 brightness = 0  # how bright the LED is
 fade_amount = 1285  # 2% stepping of 2^16
+
+pixel_pin = board.D1
+num_pixels = 7
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.33, auto_write=False, pixel_order=neopixel.GRBW)
+BLACK = (0, 0, 0, 0)
+wheel_count = 0
 
 
 def sequin_pulse():
@@ -25,6 +32,32 @@ def sequin_pulse():
         counter += 1
 
 
+# Helper to give us a nice color swirl
+def wheel():
+    global pixels, wheel_count
+    pos = wheel_count
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+
+    if pos < 85:
+        color = [int(pos * 3), int(255 - (pos * 3)), 0]
+    elif pos < 170:
+        pos -= 85
+        color = [int(255 - pos * 3), 0, int(pos * 3)]
+    else:
+        pos -= 170
+        color = [0, int(pos * 3), int(255 - pos * 3)]
+
+    wheel_count = (wheel_count + 1) % 256  # run from 0 to 255
+    pixels[0] = color
+
+
 while True:
-    time.sleep(0.015)
+    pixels.fill(BLACK)
+
+    wheel()
     sequin_pulse()
+
+    pixels.show()
+
+    time.sleep(0.015)
